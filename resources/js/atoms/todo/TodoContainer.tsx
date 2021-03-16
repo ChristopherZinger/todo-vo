5808eb507cb02d574d1c713633ef9af028e68fa7
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-16 23:54:32 +0100

    correct styles

--- resources/js/atoms/todo/TodoContainer.tsx
+++ resources/js/atoms/todo/TodoContainer.tsx
@@ -2,11 +2,8 @@ import styled from "styled-components"
 import { Colors } from "../style-guide"
 
 export const TodoContainerStyled = styled.div`
-  position: relative;
-  display: block;
+  margin: auto;
   padding-top: 20px;
-  left: 50%;
-  transform: translateX(-50%);
   width: 369px;
   border-radius: 10px 10px 0 0;
   background-color: ${Colors.UI00};
