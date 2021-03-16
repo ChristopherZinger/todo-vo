24773bfb6006032837e3af6005d03245942b86a4
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-16 15:47:41 +0100

    create get todo hook

---
+++ resources/js/apiHooks/useGetTodoList.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { getTodoList } from "../adapters/todoAdapters";
+import { ITodoList } from "../types";
+
+export const useGetTodoList = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState<ITodoList>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const data = await getTodoList();
+        setLoading(false);
+        setData(data);
+      } catch (err) {
+        setLoading(false)
+        setError(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { loading, error, data };
+};
