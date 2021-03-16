328767a5deb0fd5bbdde80d9fcd3ebd6dcbadb1c
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-16 23:49:30 +0100

    create styled circle btn

--- resources/js/atoms/circleBtn/CircleBtn.tsx
+++ resources/js/atoms/circleBtn/CircleBtn.tsx
@@ -9,6 +9,24 @@ export const CircleBtn = styled.div`
   height: 65px;
   width: 65px;
   left: 50%;
-  /* bottom: 50px; */
   transform: translateX(-50%);
+
+  span{
+    width:20px;
+    height: 2px;
+    background-color: white;
+    transform-origin: 50% 50%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    display: block;
+  }
+
+  span:last-child{
+    transform: translate(-50%, -50%);
+  }
+
+  span:first-child{
+    transform:translate(-50%, -50%) rotate(90deg) ;
+  }
 `
