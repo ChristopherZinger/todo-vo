ae71d924f964426f437a04b0ae47677a0174c8ba
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-16 15:42:11 +0100

    add get todo and updateTodo adapter

--- resources/js/adapters/todoAdapters.ts
+++ resources/js/adapters/todoAdapters.ts
@@ -1,8 +1,18 @@
 import axios from "axios";
 import { todoDM } from "../datamapper/todo/todoDM";
-import { ITodoList } from "../types";
+import { ITodoList, ITodo } from "../types";
 
 export const getTodoList = async (): Promise<ITodoList> => {
-    const data = await axios.get<ITodoList>("/api/todos");
-    return todoDM.convertAPIListResponse(data.data);
+    const response = await axios.get<ITodoList>("/api/todos");
+    return todoDM.convertAPIListResponse(response.data);
+};
+
+export const getTodo = async (id: number): Promise<ITodo> => {
+    const response = await axios.get<ITodo>(`/api/todos/${id}`);
+    return todoDM.convertAPIDetailResponse(response.data);
+};
+
+export const updateTodo = async (id: number, data: ITodo): Promise<ITodo> => {
+    const response = await axios.put<ITodo>(`/api/todos/${id}`, data);
+    return todoDM.convertAPIDetailResponse(response.data);
 };
