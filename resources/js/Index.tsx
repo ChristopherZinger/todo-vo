4ab556187fd01bb7bf58005ac2d6de3619485f13
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-16 23:48:57 +0100

    add global style comp to index

--- resources/js/Index.tsx
+++ resources/js/Index.tsx
@@ -1,9 +1,13 @@
 import { Root } from 'postcss';
 import React from 'react';
+import { GlobalStyles } from './atoms/GlobalStyles';
 import { AppRoutes } from "./pages/AppRoutes";
 
 const Index = () => {
-  return <AppRoutes />;
+  return <>
+    <GlobalStyles />
+    <AppRoutes />
+  </>;
 };
 
 export default Index;
