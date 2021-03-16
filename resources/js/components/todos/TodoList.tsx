909b1fca25db69652894defcd3a91d83a94c9881
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-16 15:45:05 +0100

    display todo list with context data, api response to context data

--- resources/js/components/todos/TodoList.tsx
+++ resources/js/components/todos/TodoList.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import { TodoCard } from "./TodoCard";
-import { useGetTodoList } from "../../apiHooks/todoAPIHooks";
+import { useGetTodoList } from "../../apiHooks/useGetTodoList";
 import { TodoContainerStyled } from "../../atoms/todo/TodoContainer";
+import { TodoListActions, TodoListContext } from "../../context/todoContext/TodoContext";
 
 export const TodoList = () => {
   const { loading, error, data } = useGetTodoList();
+  const todoList = useContext(TodoListContext);
+  const todoActions = useContext(TodoListActions)
+
+  useEffect(() => {
+    todoActions?.setFullList(data)
+  }, [data])
 
   return (
     <TodoContainerStyled>
@@ -12,8 +19,8 @@ export const TodoList = () => {
         <>loading</>
       )}
 
-      {data &&
-        data.map((todo) => <TodoCard key={todo.id} todo={todo} />)
+      {todoList &&
+        todoList.map((todo) => <TodoCard key={todo.id} todo={todo} />)
       }
     </TodoContainerStyled>
   )
