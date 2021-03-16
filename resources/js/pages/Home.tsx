6315c49a76ebbacc3e8e554d6fcae1f7e5c69e1d
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-16 15:50:02 +0100

    use todo context wrapper in Home.tsx

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Navbar } from "../components/navbar/Navbar";
 import { TodoList } from "../components/todos/TodoList";
 import { Colors } from "../atoms/style-guide";
+import { TodoContextWrapper } from "../context/todoContext/TodoContext";
 
 export const Home = () => {
   return (
     <div style={{
       background: `linear-gradient(to bottom, ${Colors.BRAND01} 257px,${Colors.BRAND01} 257px,${Colors.BRAND01} 257px,${Colors.UI00} 257px,${Colors.UI00} 100%)`
     }}>
-      <Navbar />
-      <TodoList />
+      <TodoContextWrapper>
+        <Navbar />
+        <TodoList />
+      </TodoContextWrapper>
     </div>
   )
 }
