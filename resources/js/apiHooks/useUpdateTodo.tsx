2c5a1d7b1813a46f0ea3d371a15978b6d1b984c7
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-16 17:42:54 +0100

    remove redundant imports

--- resources/js/apiHooks/useUpdateTodo.tsx
+++ resources/js/apiHooks/useUpdateTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { updateTodo } from "../adapters/todoAdapters";
 import { ITodo } from "../types";
 
