67c57b6da8ad54190c3c089cdca3a47eb6665023
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-16 15:49:35 +0100

    develop update status icon with context and api call

--- resources/js/components/todos/TodoStatusIcon.tsx
+++ resources/js/components/todos/TodoStatusIcon.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
-import {
-  IconLookup,
-  IconDefinition,
-  findIconDefinition
-} from '@fortawesome/fontawesome-svg-core'
+import { faCheckCircle, faCircle, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { useUpdateTodo } from "../../apiHooks/useUpdateTodo";
+import { ITodo } from "../../types";
+import { TodoListActions } from "../../context/todoContext/TodoContext";
 
-const coffeeLookup: IconLookup = { prefix: 'fal', iconName: 'coffee' }
-const coffeeIconDefinition: IconDefinition = findIconDefinition(coffeeLookup)
-
-export const TodoStatusIcon = (props: { done: boolean }) =>
-  <div>
-    {props.done ? <FontAwesomeIcon icon={faCheckCircle} color="gray" /> : "pending"}
-  </div>
+export const TodoStatusIcon = (props: { todo: ITodo }) => {
+  const [updateTodo, { loading, data, error }] = useUpdateTodo();
+  const todoActions = useContext(TodoListActions);
+  return (
+    <>
+      {loading && (
+        <div>
+          <FontAwesomeIcon icon={faSpinner} color="gray" />
+        </div>
+      )}
+      {!loading && (
+        <div style={{ cursor: "pointer" }} onClick={async () => {
+          if (!loading) {
+            const newTodo = await updateTodo(props.todo.id, { ...props.todo, done: !props.todo.done });
+            if (newTodo && todoActions) {
+              todoActions.updateTodo(newTodo)
+            }
+          }
+        }}>
+          {props.todo.done ? <FontAwesomeIcon icon={faCheckCircle} color="gray" /> : <FontAwesomeIcon icon={faCircle} color="gray" />}
+        </div>
+      )}
+    </>
+  )
+}
