840c9987929890cd69d6f9ea7ce0c34d89233431
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-16 23:48:30 +0100

    add colors to style-guide

--- resources/js/atoms/style-guide.ts
+++ resources/js/atoms/style-guide.ts
@@ -1,7 +1,10 @@
 export const Colors = {
     ERROR: "#E43F28",
     BRAND01: "#003CFB",
+    BRAND02: "#B89B36",
+    BRAND03: "#FAF6E9",
     UI00: "#FFFFFF",
     UI01: "#E0E0E0",
     UI02: "#ECECEC",
+    UI03: "#707070",
 };
