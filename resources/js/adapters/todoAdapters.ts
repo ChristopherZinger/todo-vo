f8cd580b6d050d9fa304e92e90a708664186c492
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-03-17 08:54:49 +0100

    remove redundant hook and adaper

--- resources/js/adapters/todoAdapters.ts
+++ resources/js/adapters/todoAdapters.ts
@@ -7,11 +7,6 @@ export const getTodoList = async (): Promise<ITodoList> => {
     return todoDM.convertAPIListResponse(response.data);
 };
 
-export const getTodo = async (id: number): Promise<ITodo> => {
-    const response = await axios.get<ITodo>(`/api/todos/${id}`);
-    return todoDM.convertAPIDetailResponse(response.data);
-};
-
 export const updateTodo = async (id: number, data: ITodo): Promise<ITodo> => {
     const response = await axios.put<ITodo>(`/api/todos/${id}`, data);
     return todoDM.convertAPIDetailResponse(response.data);
